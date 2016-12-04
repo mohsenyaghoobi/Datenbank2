67579430f0eb222007a54eb9e318cb37ac372bc1
Mohsen_4050 <Mohsen_4050@yahoo.com>
2016-12-04 18:46:26 +0100

    Mit Größe der VB und Diff in Byte

--- Z3/src/StartMainiii.java
+++ Z3/src/StartMainiii.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
  */
 public class StartMainiii {
     public static void main(String[] args) {
+        int g1;
+        int g2;
         DEVB t = new DEVB();
         int [] list = {1,7,56,134,256,268,384,472,512,648};
         int [] list1 = new int[list.length];
@@ -12,6 +14,7 @@ public class StartMainiii {
         byte[] list3= new byte[list.length];
         int [] list4 = new int[list.length];
         list1 = t.encodeDiff(list);
+        System.out.print("i)");
         System.out.println("***Differential Encoding***");
         System.out.print("Encoded: [  ");
         for (int i=0; i<list1.length ; i++){
@@ -25,7 +28,7 @@ public class StartMainiii {
             System.out.print(list2[i] + "  ");
         }
         System.out.println("  ]");
-
+        System.out.print("ii)");
         System.out.println("***Variable Bytes Encoding***");
         list3 = t.encodeVB(list);
         System.out.print("Encoded: [  ");
@@ -40,7 +43,11 @@ public class StartMainiii {
             System.out.print(list4[i] + "  ");
         }
         System.out.println("  ]");
-
+        g1 = list1.length*4;
+        g2 = list3.length*1;
+        System.out.println("Die Größe der Differential Encoding:::"+ g1);
+        System.out.println("Die Größe der Variable Bytes Encoding:::"+ g2);
+        System.out.println("Unserer Meinung nach ist VBencoding besser als Diffencoding, weil VBencoding weniger Bytes verbraucht ");
 
     }
 
