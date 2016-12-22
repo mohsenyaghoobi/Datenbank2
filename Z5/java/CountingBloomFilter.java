27375be451e3817fcac6154bc621265efc35f01f
Jevgenij Huebert <Jevgenij Huebert>
2016-12-22 11:32:55 +0100

    aufgabe 3 fertig

---
+++ Z5/java/CountingBloomFilter.java
@@ -0,0 +1,63 @@
+package aufgabe53;
+/**
+ * 
+ */
+
+/**
+ * @author Michael Körber
+ *
+ */
+public class CountingBloomFilter<T> {
+    
+    public static enum Result { FALSE, MAYBE };
+    
+    private final int[] counts;
+    
+    private final HashFunction<T>[] hashes;
+    
+    public CountingBloomFilter( int buckets, HashFunction<T>...hashes) {
+        this.hashes = hashes;
+        this.counts = new int[buckets];
+    }
+    
+    /**
+     * Inserts the given object into this filter. Ensure, that this
+     * object was not saved before calling this method.
+     * @param obj the object to insert
+     */
+    public void insert( T obj ) {
+    	for(int i=0; i<hashes.length; i++)
+    		counts[hashes[i].hash(obj)%counts.length]++;
+    }
+    
+    /**
+     * Removes the given object from this filter. Ensure, that this
+     * object was definitely saved before calling this method.
+     * @param obj the object to remove.
+     */
+    public void remove( T obj ) {
+    	if(contains(obj)!= Result.FALSE)
+        	for(int i=0; i<hashes.length; i++)
+        		counts[hashes[i].hash(obj)%counts.length]--;
+    }
+    
+    
+    /**
+     * Checks if the given object is possibly stored in this bloom filter.
+     * @param obj the object to check
+     * @return {@link Result#FALSE}, if the given object is not stored inside the filter, {@link Result#MAYBE} otherwise
+     */
+    public Result contains( T obj ) {
+    	Result r = Result.FALSE;
+    	for(int i=0; i<hashes.length; i++){
+    		int h = counts[hashes[i].hash(obj)%counts.length];
+    		if(h>0)
+    			r = Result.MAYBE;
+    		else if(h == 0)
+    			return Result.FALSE;
+    	}
+    	
+        return r;
+    }
+
+}
---
+++ Z5/java/Main.java
@@ -0,0 +1,43 @@
+package aufgabe53;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HashFunction<Integer> h = new HashFunction<Integer>() {
+			@Override
+			public int hash(Integer obj) {
+				return obj.intValue()*3+7;
+			}
+		};
+		HashFunction<Integer> i = new HashFunction<Integer>() {
+			@Override
+			public int hash(Integer obj) {
+				return obj.intValue()*13+3;
+			}
+		};
+		HashFunction<Integer> j = new HashFunction<Integer>() {
+			@Override
+			public int hash(Integer obj) {
+				return obj.intValue()*7+4;
+			}
+		};
+		// 10, 5, 6, 8, 7, 1, 3, 5, 6, 4
+		BloomSet<Integer> bs = new BloomSet<Integer>(10, h, i, j);
+		bs.add(10);
+		bs.add(5);
+		bs.add(6);
+		bs.add(8);
+		bs.add(7);
+		bs.add(1);
+		bs.add(3);
+		bs.add(5);
+		bs.add(6);
+		bs.add(4);
+		// 14, 9, 3
+		System.out.println(bs.contains(14)); // true  (false)
+		System.out.println(bs.contains(9));  // true  (false)
+		System.out.println(bs.contains(3));  // true  (true)
+		
+		//Es kann nur die 3 richtig von Bloom Filter beantwortet werden,
+	}
+}
