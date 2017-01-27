c685c72ba1605f2684591caf623dd9f8d5408f75
QWERT\qwert <iwnfubb@googlemail.com>
2017-01-27 12:21:43 +0100

    A2 Teil a fixed

---
+++ Z6/src/Main.java
@@ -0,0 +1,23 @@
+import java.sql.SQLException;
+
+import xxl.core.indexStructures.builder.BPlusTree.ManagedType;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		//Aufgabe a)
+		RelationCursor r = new RelationCursor("company_country.tsv");
+
+		RelationCursor s = new RelationCursor("person_company.tsv");
+
+		Join join = new Join(r, s);
+		
+		while(join.hasNextObject())
+			System.out.println(join.nextObject());
+		r.close();
+		s.close();
+		// TODO Aufgabe b und c)
+		//Companies c = new Companies();
+	}
+
+}
